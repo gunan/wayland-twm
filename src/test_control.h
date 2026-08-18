c2efac37d257fe8d11de16e8e37b36d23429b142
Gunhan Gulsoy <gunhangulsoy@gmail.com>
2026-08-18 08:06:53 -0700

    test-control: parse output topology mutations

--- src/test_control.h
+++ src/test_control.h
@@ -8,6 +8,28 @@
 
 #define WTWM_TEST_CONTROL_TEXT_MAX 512
 
+enum wtwm_test_output_operation {
+	WTWM_TEST_OUTPUT_ADD,
+	WTWM_TEST_OUTPUT_DESTROY,
+	WTWM_TEST_OUTPUT_ENABLE,
+	WTWM_TEST_OUTPUT_DISABLE,
+	WTWM_TEST_OUTPUT_MODE,
+	WTWM_TEST_OUTPUT_SCALE,
+	WTWM_TEST_OUTPUT_TRANSFORM,
+	WTWM_TEST_OUTPUT_POSITION,
+};
+
+enum wtwm_test_output_transform {
+	WTWM_TEST_TRANSFORM_NORMAL,
+	WTWM_TEST_TRANSFORM_90,
+	WTWM_TEST_TRANSFORM_180,
+	WTWM_TEST_TRANSFORM_270,
+	WTWM_TEST_TRANSFORM_FLIPPED,
+	WTWM_TEST_TRANSFORM_FLIPPED_90,
+	WTWM_TEST_TRANSFORM_FLIPPED_180,
+	WTWM_TEST_TRANSFORM_FLIPPED_270,
+};
+
 enum wtwm_test_command_type {
 	WTWM_TEST_COMMAND_PING,
 	WTWM_TEST_COMMAND_OUTPUT,
@@ -27,12 +49,17 @@ enum wtwm_test_command_type {
 
 struct wtwm_test_command {
 	enum wtwm_test_command_type type;
+	enum wtwm_test_output_operation output_operation;
+	enum wtwm_test_output_transform output_transform;
 	int first;
 	int second;
+	int third;
 	double x;
 	double y;
 	uint32_t code;
 	bool pressed;
+	bool output_auto;
+	char output_name[WTWM_TEST_CONTROL_TEXT_MAX];
 	char text[WTWM_TEST_CONTROL_TEXT_MAX];
 };
 
